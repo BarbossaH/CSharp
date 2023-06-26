ece848f6cb81e67a61049d2462945ff92f1388b9
Julian <huangbobeijing2008@gmail.com>
2023-06-26 23:19:22 +1200

    add comments

--- Firstone/FormatData.cs
+++ Firstone/FormatData.cs
@@ -5,10 +5,10 @@ namespace Firstone
 	{
 		static void Main(string[] arg)
 		{
-			Console.WriteLine("Currency : {0:c}", 23.323223);
-			Console.WriteLine("Pad with 0s : {0:d4}",33);
-			Console.WriteLine("3 Decimals : {0:f4}", 23.1234567);
-			Console.WriteLine("Commas : {0:n4}", 3300);
+			Console.WriteLine("Currency : {0:c}", 23.323223);//$23.32
+			Console.WriteLine("Pad with 0s : {0:d4}",33);//0033
+			Console.WriteLine("3 Decimals : {0:f4}", 23.1234567); //23.1235
+			Console.WriteLine("Commas : {0:n4}", 3300); // 3300.000
 			Console.WriteLine("Commas : {0} is {1} ", "My name","Julian");
         }
 	}
