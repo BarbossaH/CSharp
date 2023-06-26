7bdc445b5f6c067feb1fb7210f82f10fc6b9fc2c
Julian <huangbobeijing2008@gmail.com>
2023-06-26 23:48:53 +1200

    add format

--- Firstone/FormatFunction.cs
+++ Firstone/FormatFunction.cs
@@ -1,16 +1,1 @@
-﻿using System;
-namespace Firstone
-{
-	public class FormatData
-	{
-		static void Main(string[] arg)
-		{
-			Console.WriteLine("Currency : {0:c}", 23.323223);//$23.32
-			Console.WriteLine("Pad with 0s : {0:d4}",33);//0033
-			Console.WriteLine("3 Decimals : {0:f4}", 23.1234567); //23.1235
-			Console.WriteLine("Commas : {0:n4}", 33000); // 3300.000
-			Console.WriteLine("Commas : {0} is {1} ", "My name","Julian");
-        }
-	}
-}
-
+﻿
