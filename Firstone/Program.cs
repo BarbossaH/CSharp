23c0b484a289dcfc87f33956890dac6a1a435477
Julian <huangbobeijing2008@gmail.com>
2023-06-26 22:02:26 +1200

    create a c# project

---
+++ Firstone/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace FirstApp
+{
+    public class FirstProgram
+    {
+        static void Main(string[] args) {
+
+            //bool isVote = true;
+            //Console.WriteLine("Biggest Integer : {0}", int.MaxValue);
+            //Console.WriteLine("Biggest Integer : {0}", int.MinValue);
+            //Console.WriteLine("Biggest Long : {0}", long.MaxValue);
+            //Console.WriteLine("Biggest Long : {0}", long.MinValue);
+            //decimal delNumber = 0.333323232M;
+            //Console.WriteLine("Biggest Decimal : {0}", decimal.MaxValue);
+            //Console.WriteLine("Biggest Decimal : {0}", decimal.MinValue);
+            //Console.WriteLine("Biggest double : {0}", double.MinValue);
+            //Console.WriteLine("Biggest double : {0}", double.MaxValue);
+            //double dNum = 0.444;
+            //float fNum = 0.33f;
+            //Console.WriteLine("Biggst Float :{0}", float.MaxValue);
+            //Console.WriteLine("Smallest Float :{0}", float.MinValue);
+            /*
+             *  byte, char, 
+             */
+        }
+    }
+}
